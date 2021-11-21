dbc66903fa0d329462f7fc81084151c88d225ad7
mahmoud947 <65209396+mahmoud947@users.noreply.github.com>
2021-11-21 19:20:25 +0200

    added category search

---
+++ api/cars/GetAllCarsInCatagory.php
@@ -0,0 +1,23 @@
+<?php
+require_once '../../operation/CarOperation.php';
+$response = array();
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    if (isset($_GET['car_category'])) {
+
+        $db = new DBCarsOperations();
+        if ($db->getAllCarsInfoByCategory($_GET['car_category'])) {
+            $response = $db->getAllCarsInfoByCategory($_GET['car_category']);
+            echo json_encode($response, JSON_PRETTY_PRINT);
+        } else {
+            $response['error'] = true;
+            $response['message'] = "car not found";
+            echo json_encode($response);
+        }
+    } else {
+        $response['error'] = true;
+        $response['message'] = "Require fields are missing";
+        echo json_encode($response, JSON_PRETTY_PRINT);
+    }
+}
+ 
--- operation/CarOperation.php
+++ operation/CarOperation.php
@@ -98,6 +98,53 @@ class DBCarsOperations
 
 
 
+
+
+
+
+    public function getAllCarsInfoByCategory($category_name)
+    {
+        $stm = $this->con->query(
+            "SELECT car.car_id,  car.car_name, car.motor_capacity,
+            car.mechanical_horse,car.model, car.number_of_set,car.tank_size,car.price,car.count,car.turbo,car.model_year
+            ,car_category.category_name as category,admin.user_name as admin_name
+            FROM car
+            INNER JOIN admin
+           ON car.admin_id=admin.admin_id
+			 INNER JOIN car_category
+           ON car.category_id=car_category.category_id AND car.category_id=(SELECT category_id FROM car_category WHERE car_category.category_name='$category_name')"
+        );
+        $carsArray = array();
+
+        while ($row = $stm->fetch_assoc()) {
+            extract($row);
+            $e = array(
+                "Id" => $car_id,
+                "category" => $category,
+                "name" => $car_name,
+                "motor_capacity" => $motor_capacity,
+                "mechanical_horse" => $mechanical_horse,
+                "model" => $model,
+                "model_year" => $model_year,
+                "number_of_set" => $number_of_set,
+                "tank_size" => $tank_size,
+                "price" => $price,
+                "admin" => $admin_name,
+                "count" => $count,
+                "turbo" => $turbo,
+                "cover" => $this->getCoverImagebyCarID($car_id),
+                "image" => $this->getOtherImagebyCarID($car_id)
+            );
+
+            array_push($carsArray, $e);
+        }
+        return $carsArray;
+    }
+
+
+
+
+
     public function getCoverImagebyCarID($car_id)
     {
         $stm = $this->con->query("SELECT * FROM car_image WHERE car_id=$car_id");
@@ -120,4 +167,7 @@ class DBCarsOperations
         }
         return $imageArray;
     }
+
+
+   
 }
